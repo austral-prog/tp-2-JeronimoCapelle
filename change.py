9965cf673e5c86cad8a0d7e0b986b0aff93e7856
JeronimoCapelle <jcapelle@mail.austral.edu.ar>
2025-03-28 11:02:28 -0300

    local test okay

--- change.py
+++ change.py
@@ -1,3 +1,11 @@
 def change():
     expense = 23.75
     money = 100
+
+    expense = float(input("Ingresar gasto:\n"))
+    print(expense)
+    money = float(input("Dinero recibido\n"))
+    print(int(money))
+    
+    print
+    print(f"\nVuelto\n\nPesos:\n{int(money - expense)}\nCentavos:\n{int(((money - expense) % 1)*100)}")
